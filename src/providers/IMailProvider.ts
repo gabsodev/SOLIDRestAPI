558b73839d7b54d920227b06a54bc2f17c26f47a
Gabriel Mendes <gabrieltkd2013@gmail.com>
2022-05-05 02:03:42 -0300

    feat: Created a user sign in system based in SOLID pattern.

---
+++ src/providers/IMailProvider.ts
@@ -0,0 +1,15 @@
+export interface IAddres {
+  email: string;
+  name: string;
+}
+
+export interface IMessage {
+  to: IAddres;
+  from: IAddres;
+  subject: string;
+  body: string;
+}
+
+export interface IMailProvider {
+  sendMail(message: IMessage): Promise<void>;
+}
---
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+
+export class CreateUserUseCase {
+  constructor(
+    private userRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
+  
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.userRepository.findByEmail(data.email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists.');
+    }
+
+    const user = new User(data);
+
+    await this.userRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email
+      },
+      from: { 
+        name: 'Equipe do Gabs',
+        email: 'equipe@dogabs.com'
+      },
+      subject: 'Bem-vindo à plataforma!',
+      body: '<p>Você já pode fazer login</p>'
+    })
+  }
+}
